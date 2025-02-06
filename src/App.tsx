18e67b7a91e7d36d7262449b5d988b534414e928
gbogdanova <gaaalo44ka@gmail.com>
2025-02-06 10:53:43 -0800

    feat: add routing and 404 page for non-existing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Header from './components/Header';
-import Main from './components/Main';
 import InfProvider from './store/planets-proveder';
 import ErrorBoundary from './components/ErrorBoundary';
 import TestBtn from './components/TestBtn';
+import { Routes, Route } from 'react-router';
+import NotFound from './components/NotFound';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <ErrorBoundary>
       <InfProvider>
-        <Header />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </InfProvider>
       <TestBtn />
     </ErrorBoundary>
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { BrowserRouter } from 'react-router';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        {' '}
+        <App />
+      </BrowserRouter>
     </StrictMode>
   );
 }
