3b74885c2fab9c0a7684a4cae6675b3b20e7f187
gbogdanova <gaaalo44ka@gmail.com>
2025-03-18 15:28:54 -0700

    feat: fetch and display data

---
+++ src/components/CardList.tsx
@@ -0,0 +1,32 @@
+import Country from '../interfaces';
+
+interface CardListProp {
+  countries: Country[];
+}
+export default function CardList({ countries }: CardListProp) {
+  return (
+    <>
+      {countries.length > 0 ? (
+        <ul>
+          {countries.map((country, ind) => (
+            <li key={ind}>
+              <div>{country.name.official}</div>
+              <div>{country.name.common}</div>
+              <div>{country.region}</div>
+              <div>{country.population}</div>
+              <div>
+                <img
+                  src={country.flags.png}
+                  alt={country.name.official}
+                  width="100"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>There is no countries</p>
+      )}
+    </>
+  );
+}
