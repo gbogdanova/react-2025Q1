9c1a49ee6cb311cac9c3224587684fe0bfeb5abe
gbogdanova <gaaalo44ka@gmail.com>
2025-03-21 12:12:34 -0700

    feat: add sort countries by population or name

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Country from './interfaces';
 import CardList from './components/CardList';
 import Search from './components/Search';
 import Filter from './components/Filter';
+import Sort from './components/Sort';
 
 export default function App() {
   const [countries, setCountries] = useState<Country[]>([]);
   const [region, setRegion] = useState<string>('');
   const [search, setSearch] = useState<string>('');
+  const [sort, setSort] = useState<string>('');
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -27,12 +29,29 @@ export default function App() {
   }, []);
 
   const filterCountries = useMemo(() => {
-    return countries.filter((country) => {
+    const filtered = countries.filter((country) => {
       const matchesRegion = region ? country.region === region : true;
       const matchSearch = search ? country.name.common.includes(search) : true;
       return matchesRegion && matchSearch;
     });
-  }, [countries, region, search]);
+
+    switch (sort) {
+      case '1':
+        return filtered.sort((a, b) => a.population - b.population);
+      case '2':
+        return filtered.sort((a, b) => b.population - a.population);
+      case '3':
+        return filtered.sort((a, b) =>
+          a.name.common.localeCompare(b.name.common)
+        );
+      case '4':
+        return filtered.sort((a, b) =>
+          b.name.common.localeCompare(a.name.common)
+        );
+      default:
+        return filtered;
+    }
+  }, [countries, region, search, sort]);
 
   const handleSearch = (searchQuary: string) => {
     setSearch(searchQuary);
@@ -41,11 +60,18 @@ export default function App() {
   const handleFilter = (selectedRegion: string) => {
     setRegion(selectedRegion === 'all' ? '' : selectedRegion);
   };
+  const handleSort = (sort: string) => {
+    setSort(sort);
+  };
 
   return (
     <>
-      <Filter onFilter={handleFilter} />
-      <Search onSearch={handleSearch} />
+      <header className="flex justify-between mb-6">
+        <Sort onSort={handleSort} />
+        <Filter onFilter={handleFilter} />
+        <Search onSearch={handleSearch} />
+      </header>
+
       <CardList countries={filterCountries} />
     </>
   );
---
+++ src/components/Sort.tsx
@@ -0,0 +1,24 @@
+interface SortProps {
+  onSort: (sort: string) => void;
+}
+
+export default function Sort({ onSort }: SortProps) {
+  return (
+    <div>
+      <label htmlFor="sort">Sort:</label>
+      <select
+        name="sort"
+        defaultValue=""
+        onChange={(e) => {
+          onSort(e.target.value);
+        }}
+      >
+        <option value="">No Sort</option>
+        <option value="1">Population ↑</option>
+        <option value="2">Population ↓</option>
+        <option value="3">Name A–Z</option>
+        <option value="4">Name Z–A</option>
+      </select>
+    </div>
+  );
+}
